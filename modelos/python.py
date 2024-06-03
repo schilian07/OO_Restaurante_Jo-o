21389dcf3335ea9293d494703c37041964d7f77d
schilian07 <161601193+schilian07@users.noreply.github.com>
2024-06-03 09:51:52 -0300

    Add files via upload

---
+++ modelos/python.py
@@ -0,0 +1,37 @@
+class Restaurante:
+    nome = ""
+
+    #Questão 4
+    categoria=""
+    ativo = False
+
+restaurante_praca = Restaurante()
+
+#Questão 2-5
+restaurante_praca.nome ="Bistrô"
+
+#Questão 1
+restaurante_praca.categoria="Italiano"
+
+#Questão 3
+restaurante_praca.ativo="Ativo"
+restaurante_pizza = Restaurante()
+
+#Questão 6
+restaurante_pizza.nome ="PizzaPlace"
+
+#Questão 7
+restaurante_pizza.categoria="Fast-Food"
+
+#Questão 8
+restaurante_pizza.ativo="Ativo"
+
+
+
+
+
+Restaurante=[restaurante_praca, restaurante_pizza]
+
+#Questão 9
+print(" ")
+print(vars( restaurante_praca))
